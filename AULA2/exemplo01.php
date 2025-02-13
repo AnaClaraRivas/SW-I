1a26ef7cb4ef64ed2f87d025dae1c81de789cb05
Ana Clara Bronzeri Rivas <159500946+AnaClaraRivas@users.noreply.github.com>
2025-02-13 08:55:07 -0300

    Add files via upload

---
+++ AULA2/exemplo01.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $nome = "Esnupi";
+    ?>
+    <p> Ana here bbs </p>
+
+    <?php
+        echo "<p> Omg!!! </p>";
+        echo "<p> Oi sou o cachorrinho $nome </p>";
+        echo "<p>Continuo sendo o " .$nome. "</p>";
+    ?>
+    
+</body>
+</html>
