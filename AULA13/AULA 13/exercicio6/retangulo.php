d5e6327f72cec28ac28474f98acc68e631b6e084
Ana Clara Bronzeri Rivas <159500946+AnaClaraRivas@users.noreply.github.com>
2025-06-17 19:31:55 -0300

    Delete AULA13/AULA 13 directory

--- AULA13/AULA 13/exercicio6/retangulo.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include_once 'Retangulo.Class.php';
-
-    $retangulo = new Retangulo();
-    $retangulo->setLargura(9);
-    $retangulo->setAltura(3);
-
-    $retangulo->exibirResultados();
-?>
